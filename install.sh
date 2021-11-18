14c35bc7a8022ccfe9905bf290c046a01d339247
Pawel Wenda <pawel@wenda.eu>
2021-11-18 13:50:31 +0000

    add install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+link () {
+	echo "[+] Creating symbolic link for: $1"
+	ln -svr $1 ~
+}
+
+echo "[+] Making vim undo directory"
+mkdir -p ~/.vim/undodir
+
+link .gitconfig
+link .inputrc
+link .vimrc
+link .tmux.conf
+link .zshrc
+link .bashrc
+
+
