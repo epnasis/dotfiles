c9a2183e83fc6d5841372798bd8b8bf72862708e
Pawel Wenda <pawel@wenda.eu>
2026-01-11 14:44:48 +0100

    feat: add 1password as ssh-agent for key forwarding
    env variable required for key forwarding over ssh

--- home/.shell.d/70-1password.sh
+++ home/.shell.d/70-1password.sh
@@ -1,6 +1,9 @@
 # 1Password CLI
 command -v op >/dev/null || return
 
+# SSH agent for forwarding keys
+export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+
 if [[ -n "$ZSH_VERSION" ]]; then
   eval "$(op completion zsh)"
   compdef _op op
