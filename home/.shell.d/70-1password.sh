75cbd462dd1cf9b34dc9e8faadb9aeb1245b76a4
Pawel Wenda <pawel@wenda.eu>
2026-01-11 22:19:36 +0100

    fix: ssh agent forwarding disabled if already in ssh session

--- home/.shell.d/70-1password.sh
+++ home/.shell.d/70-1password.sh
@@ -1,14 +1,16 @@
 # 1Password CLI
 command -v op >/dev/null || return
 
-# SSH agent for forwarding keys
-export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+# SSH agent for forwarding keys only if not in SSH already
+if [ -z "$SSH_CONNECTION" ]; then
+	export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+fi
 
 if [[ -n "$ZSH_VERSION" ]]; then
 	OP_COMPLETION_CACHE="${XDG_CACHE_HOME:-$HOME/.cache}/op_completion.zsh"
 	if [[ ! -f "$OP_COMPLETION_CACHE" ]]; then
 		mkdir -p "$(dirname "$OP_COMPLETION_CACHE")"
-		op completion zsh > "$OP_COMPLETION_CACHE"
+		op completion zsh >"$OP_COMPLETION_CACHE"
 	fi
 	source "$OP_COMPLETION_CACHE"
 	compdef _op op
