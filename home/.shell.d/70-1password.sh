67fdd9d3e16ed9758ad229f8e34d4505c05a4a1a
Pawel Wenda <pawel@wenda.eu>
2026-01-11 14:46:08 +0100

    fix: improve 1p loading speed
    no need to refresh 1p completion cache if it exist already - dramatic
    improvement

--- home/.shell.d/70-1password.sh
+++ home/.shell.d/70-1password.sh
@@ -5,10 +5,15 @@ command -v op >/dev/null || return
 export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
 
 if [[ -n "$ZSH_VERSION" ]]; then
-  eval "$(op completion zsh)"
-  compdef _op op
+	OP_COMPLETION_CACHE="${XDG_CACHE_HOME:-$HOME/.cache}/op_completion.zsh"
+	if [[ ! -f "$OP_COMPLETION_CACHE" ]]; then
+		mkdir -p "$(dirname "$OP_COMPLETION_CACHE")"
+		op completion zsh > "$OP_COMPLETION_CACHE"
+	fi
+	source "$OP_COMPLETION_CACHE"
+	compdef _op op
 elif [[ -n "$BASH_VERSION" ]]; then
-  eval "$(op completion bash)"
+	eval "$(op completion bash)"
 fi
 
 # 1Password plugins
