890e3fca3ae5814342f862864018d1e714a3fb64
Pawel Wenda <pawel@wenda.eu>
2026-01-11 22:20:00 +0100

    fix: start tmux once and exit if no errors

--- home/.shell.d/99-tmux-ssh.sh
+++ home/.shell.d/99-tmux-ssh.sh
@@ -1,6 +1,4 @@
 # Auto-start tmux on SSH sessions
-[[ -z "$SSH_CONNECTION" ]] && return
-[[ -n "$TMUX" ]] && return
-command -v tmux >/dev/null || return
-
-tmux attach -t main 2>/dev/null || tmux new -s main
+if [ -n "$SSH_CONNECTION" ] && [ -z "$TMUX" ] && [[ $- == *i* ]]; then
+	tmux new-session -A -s main && exit 0
+fi
