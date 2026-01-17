b64191eecfc0373e062ba49f60beb84c2f7249dc
Pawel Wenda <pawel@wenda.eu>
2026-01-17 21:29:48 +0100

    feat: alias for reboot in mac keeping authenticated user for headless

--- home/.shell.d/65-macos.sh
+++ home/.shell.d/65-macos.sh
@@ -6,6 +6,8 @@ if [[ "$(uname -m)" == "arm64" ]] && command -v arch >/dev/null; then
   alias brew="arch -arm64 brew"
 fi
 
+alias reboot="sudo fdesetup authrestart"
+
 # Google Cloud SDK
 [[ -f "$HOME/google-cloud-sdk/path.zsh.inc" ]] && source "$HOME/google-cloud-sdk/path.zsh.inc"
 [[ -f "$HOME/google-cloud-sdk/completion.zsh.inc" ]] && source "$HOME/google-cloud-sdk/completion.zsh.inc"
